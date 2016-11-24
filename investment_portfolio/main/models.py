3381463c3ee6fd2eb063ea70ab8e72c3d9757c1c
Jared Brooks <brooksjaredc@gmail.com>
2016-11-23 17:03:02 -0800

    Finishes Try Django course

--- investment_portfolio/main/models.py
+++ investment_portfolio/main/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Stock(models.Model):
+	stock = models.CharField(max_length=5)
+	price = models.DecimalField(max_digits=10,decimal_places=2)
+	shares = models.DecimalField(max_digits=10,decimal_places=4)
+
+	def __str__(self):
+		return self.stock
+
+
 # Create your models here.
--- investment_portfolio/main/views.py
+++ investment_portfolio/main/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Stock
+#from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse('<h1>Hello There!</h1>')
+	stocks = Stock.objects.all()
+	#return HttpResponse('<h1>Hello There!</h1>')
+	return render(request, 'index.html', {'stocks':stocks})
+
+
+class Stock:
+	def __init__(self, stock, price, shares):
+		self.stock = stock
+		self.price = price
+		self.shares = shares
+
+stocks = [
+	Stock('INTC', 1000.00, 10),
+	Stock('MU', 20, 100),
+	Stock('INX', 2200, 2)
+]
